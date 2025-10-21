f98d2312f884081aca973d5bab8fd31592c8963d
ZiHao Liu <145265650+qs-liuzh@users.noreply.github.com>
2025-10-21 09:33:34 +0800

    next step is 'Authenticate the Client with Mutual TLS Authentication'

--- go.mod
+++ go.mod
@@ -3,11 +3,26 @@ module github.com/qs-lzh/proglog
 go 1.24.3
 
 require (
+	github.com/GeertJohan/go.rice v1.0.0 // indirect
+	github.com/cloudflare/cfssl v1.4.1 // indirect
+	github.com/daaku/go.zipexe v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.3.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/certificate-transparency-go v1.0.21 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jmhodges/clock v0.0.0-20160418191101-880ee4c33548 // indirect
+	github.com/jmoiron/sqlx v0.0.0-20180124204410-05cef0741ade // indirect
+	github.com/kisielk/sqlstruct v0.0.0-20150923205031-648daed35d49 // indirect
+	github.com/lib/pq v0.0.0-20180201184707-88edab080323 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/tysonmote/gommap v0.0.3 // indirect
+	github.com/weppos/publicsuffix-go v0.5.0 // indirect
+	github.com/zmap/zcrypto v0.0.0-20190729165852-9051775e6a2e // indirect
+	github.com/zmap/zlint v0.0.0-20190806154020-fd021b4cfbeb // indirect
+	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -8,8 +8,10 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	api "github.com/qs-lzh/proglog/api/v1"
+	"github.com/qs-lzh/proglog/internal/config"
 	"github.com/qs-lzh/proglog/internal/log"
 )
 
@@ -30,38 +32,90 @@ func TestServer(t *testing.T) {
 func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Config, teardown func()) {
 	t.Helper()
 
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
-	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile: config.CAFile,
+	})
 	require.NoError(t, err)
 
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+	cc, err := grpc.Dial(
+		l.Addr().String(),
+		grpc.WithTransportCredentials(clientCreds),
+	)
+	require.NoError(t, err)
+
+	client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+	})
+	require.NoError(t, err)
+
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 	dir, err := ioutil.TempDir("", "server-test")
 	require.NoError(t, err)
 
 	clog, err := log.NewLog(dir, log.Config{})
 	require.NoError(t, err)
 
-	cfg = &Config{CommitLog: clog}
+	cfg = &Config{
+		CommitLog: clog,
+	}
 	if fn != nil {
 		fn(cfg)
 	}
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go func() {
 		server.Serve(l)
 	}()
 
-	client = api.NewLogClient(cc)
-
 	return client, cfg, func() {
 		server.Stop()
 		cc.Close()
 		l.Close()
-		clog.Remove()
 	}
+	// t.Helper()
+	//
+	// l, err := net.Listen("tcp", ":0")
+	// require.NoError(t, err)
+	//
+	// clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	// cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	// require.NoError(t, err)
+	//
+	// dir, err := ioutil.TempDir("", "server-test")
+	// require.NoError(t, err)
+	//
+	// clog, err := log.NewLog(dir, log.Config{})
+	// require.NoError(t, err)
+	//
+	// cfg = &Config{CommitLog: clog}
+	// if fn != nil {
+	// 	fn(cfg)
+	// }
+	// server, err := NewGRPCServer(cfg)
+	// require.NoError(t, err)
+	//
+	// go func() {
+	// 	server.Serve(l)
+	// }()
+	//
+	// client = api.NewLogClient(cc)
+	//
+	// return client, cfg, func() {
+	// 	server.Stop()
+	// 	cc.Close()
+	// 	l.Close()
+	// 	clog.Remove()
+	// }
 }
 
 func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
