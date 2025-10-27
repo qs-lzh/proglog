94408142ef7740d4e960d14586fab275c560043a
ZiHao Liu <145265650+qs-liuzh@users.noreply.github.com>
2025-10-27 21:15:55 +0800

    fdjsvxc

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -34,13 +34,13 @@ type Config struct {
 	ServerTLSConfig *tls.Config
 	PeerTLSConfig   *tls.Config
 
-	DataDir  string
-	BindAddr string
-	RPCPort  int
-	NodeName string
+	DataDir        string
+	BindAddr       string
+	RPCPort        int
+	NodeName       string
 	StartJoinAddrs []string
-	ACLModelFile string
-	ACLPolicyFile string
+	ACLModelFile   string
+	ACLPolicyFile  string
 }
 
 func (c Config) RPCAddr() (string, error) {
@@ -53,11 +53,11 @@ func (c Config) RPCAddr() (string, error) {
 
 func New(config Config) (*Agent, error) {
 	a := &Agent{
-		Config: config,
+		Config:    config,
 		shutdowns: make(chan struct{}),
 	}
-	setup := []func() error {
-	  a.setupLogger,
+	setup := []func() error{
+		a.setupLogger,
 		a.setupLog,
 		a.setupServer,
 		a.setupMembership,
@@ -80,10 +80,11 @@ func (a *Agent) setupLogger() error {
 }
 
 func (a *Agent) setupLog() error {
-	a.log, err = log.NewLog(a.config.DataDir, log.Config{})
+	log, err := log.NewLog(a.Config.DataDir, log.Config{})
 	if err != nil {
 		return err
 	}
+	a.log = log
 	return err
 }
 
@@ -95,10 +96,11 @@ func (a *Agent) setupServer() error {
 		creds := credentials.NewTLS(a.Config.ServerTLSConfig)
 		opts = append(opts, grpc.Creds(creds))
 	}
-	a.server, err = server.NewGRPCServer(serverConfig, opts...)
+	server, err := server.NewGRPCServer(serverConfig, opts...)
 	if err != nil {
 		return err
 	}
+	a.server = server
 	rpcAddr, err := a.RPCAddr()
 	if err != nil {
 		return err
@@ -107,7 +109,7 @@ func (a *Agent) setupServer() error {
 	if err != nil {
 		return err
 	}
-  go func() {
+	go func() {
 		if err := a.server.Serve(listener); err != nil {
 			_ = a.Shutdown()
 		}
@@ -121,13 +123,13 @@ func (a *Agent) setupMembership() error {
 		return err
 	}
 	var opts []grpc.DialOption
-	if a.config.PeerTLSConfig != nil {
+	if a.Config.PeerTLSConfig != nil {
 		opts = append(opts,
 			grpc.WithTransportCredentials(
 				credentials.NewTLS(
-					a.Config.PeerTLSConfig
-				)
-			)
+					a.Config.PeerTLSConfig,
+				),
+			),
 		)
 	}
 	conn, err := grpc.NewClient(rpcAddr, opts...)
@@ -135,20 +137,20 @@ func (a *Agent) setupMembership() error {
 		return err
 	}
 	client := api.NewLogClient(conn)
-	a.replicator = *log.Replicator{
+	a.replicator = &log.Replicator{
 		DialOptions: opts,
 		LocalServer: client,
 	}
 	a.membership, err = discovery.New(
 		a.replicator,
 		discovery.Config{
-      NodeName: a.Config.NodeName,
+			NodeName: a.Config.NodeName,
 			BindAddr: a.Config.BindAddr,
 			Tags: map[string]string{
 				"rpc_addr": rpcAddr,
 			},
 			StartJoinAddrs: a.Config.StartJoinAddrs,
-		}
+		},
 	)
 	return err
 }
@@ -162,7 +164,7 @@ func (a *Agent) Shutdown() error {
 	a.shutdown = true
 	close(a.shutdowns)
 
-	shutdown := []func() error {
+	shutdown := []func() error{
 		a.membership.Leave,
 		a.replicator.Close,
 		func() error {
